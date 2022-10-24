5fa8fb88a2aa8f69243deff0172672a26103253d
José Manuel Nieto <superjmn@outlook.com>
2022-10-24 09:36:55 +0200

    Test for addition

--- SomeCompiler.Z80.Tests/Z80GeneratorTests.cs
+++ SomeCompiler.Z80.Tests/Z80GeneratorTests.cs
@@ -5,7 +5,6 @@ namespace SomeCompiler.Z80.Tests
 {
     public class Z80GeneratorTests
     {
-
         [Fact]
         public void Simple_test()
         {
@@ -16,6 +15,18 @@ namespace SomeCompiler.Z80.Tests
             result
                 .Should().BeSuccess()
                 .And.Subject.Value.Memory[48].Should().Be(123);
+        }
+
+        [Fact]
+        public void Addition()
+        {
+            var input = "void main() { a = 1 + 2; }";
+
+            var result = Z80Runner.Run(input);
+
+            result
+                .Should().BeSuccess()
+                .And.Subject.Value.Memory[48].Should().Be(3);
         }
     }
 }
