259a54947c3d2d4ada35ec6e60799b2466acf90a
José Manuel Nieto <superjmn@outlook.com>
2022-11-22 00:36:55 +0100

    Make Console useful

--- SomeCompiler.Cli/Program.cs
+++ SomeCompiler.Cli/Program.cs
@@ -1,10 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿void PrintSource(string source)
+{
+    PrintSection("Source code:", source);
+}
 
-using SomeCompiler.Parsing;
+void PrintSection(string header, string body)
+{
+    Console.WriteLine(header);
+    Console.WriteLine(body);
+    Console.WriteLine();
+}
 
-Console.WriteLine("Hello, World!");
+string ReadInputFile(string s)
+{
+    return File.ReadAllText(s);
+}
 
-SomeGrammar grammar = new();
-var content = grammar.ToGrammarFileContent();
-var str = string.Join(Environment.NewLine, content);
-Console.WriteLine(str);
+Result<GeneratedProgram> Generate(IntermediateCodeProgram intermediateCodeProgram)
+{
+    return new Z80Generator().Generate(intermediateCodeProgram);
+}
+
+void PrintSuccess()
+{
+    Console.Error.WriteLine("Success");
+}
+
+void PrintError(string s)
+{
+    Console.Error.WriteLine(s);
+}
+
+void PrintAssembly(GeneratedProgram generatedProgram)
+{
+    PrintSection("Z80 assembly:", generatedProgram.Assembly);
+}
+
+void PrintIL(IntermediateCodeProgram intermediateCodeProgram)
+{
+    PrintSection("Intermediate code:", intermediateCodeProgram.ToCodeFormatContent().JoinWithLines());
+}
+
+Result<IntermediateCodeProgram> Compile(string s)
+{
+    return new Compiler().Emit(s).MapError(errors => errors.JoinWithLines());
+}
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("No source file has been specified");
+    return;
+}
+
+var path = args[0];
+
+Result
+    .Try(() => ReadInputFile(path))
+    .Tap(PrintSource)
+    .Bind(Compile)
+    .Tap(PrintIL)
+    .Bind(Generate)
+    .Tap(PrintAssembly)
+    .Match(_ => PrintSuccess(), PrintError);
+    
