2887bbc2d5ca483cd0987f7912d33dd216fadeba
José Manuel Nieto <superjmn@outlook.com>
2024-02-07 23:23:28 +0100

    Implementando sugerencias

--- SomeCompiler/Binding2/IntType.cs
+++ SomeCompiler/Binding2/IntType.cs
@@ -1,44 +1,10 @@
 ﻿using CSharpFunctionalExtensions;
-using SomeCompiler.Parser.Model;
 
 namespace SomeCompiler.Binding2;
 
 public record IntType : SymbolType
 {
     public static readonly IntType Instance = new();
-}
-
-public class SemanticAnalyzer
-{
-    public (SemanticNode, List<string>) AnalyzeNode(INode node, Scope scope)
-    {
-        return node switch
-        {
-            DeclarationStatement varDecl => DeclareSymbol(varDecl, scope),
-            Block block => AnalyzeBlock(block, scope),
-            _ => throw new NotImplementedException(),
-        };
-    }
-    private (SemanticNode, List<string>) AnalyzeBlock(Block block, Scope scope)
-    {
-        var errors = new List<string>();
-        var semanticStatements = new List<SemanticNode>();
-        foreach (var stmt in block)
-        {
-            (var semanticNode, var newErrors) = AnalyzeNode(stmt, scope);
-            semanticStatements.Add(semanticNode);
-            errors.AddRange(newErrors);
-        }
-        return (new SemanticBlock(semanticStatements, scope), errors);
-    }
-    private (SemanticNode, List<string>) DeclareSymbol(DeclarationStatement decl, Scope scope)
-    {
-        var newScope = scope.TryDeclare(decl.Name, IntType.Instance)
-            .Match(
-                newScope => ((SemanticNode)new SemanticVarDecl(decl.Name, IntType.Instance, newScope), new List<string>()),
-                s => (new SemanticError(s), [s]));
-        return newScope;
-    }
 }
 
 public class SemanticError : SemanticNode
---
+++ SomeCompiler/Binding2/SemanticAnalyzer.cs
@@ -0,0 +1,86 @@
+﻿using SomeCompiler.Parser.Model;
+
+namespace SomeCompiler.Binding2;
+
+public class SemanticAnalyzer
+{
+    private (SemanticNode, Scope) AnalyzeProgram(Program node)
+    {
+        var scope = Scope.Empty;
+        var functions = new List<FunctionNode>();
+        foreach (var statement in node.Functions)
+        {
+            var (functionNode, newScope) = AnalyzeFunction(statement, scope);
+            scope = newScope;
+            functions.Add(functionNode);
+        }
+        return (new SemanticProgramNode(functions), scope);
+    }
+
+    private (FunctionNode, Scope) AnalyzeFunction(Function function, Scope scope)
+    {
+        return (new FunctionNode(function.Name, AnalyzeBlock(function.Block, scope).Item1), scope);
+    }
+    private (BlockNode, Scope) AnalyzeBlock(Block block, Scope scope)
+    {
+        return (new BlockNode(block.Select(statement => AnalyzeStatement(statement, scope).Item1)), scope);
+    }
+    private (StatementNode, Scope) AnalyzeStatement(Statement statement, Scope scope)
+    {
+        switch (statement)
+        {
+            case DeclarationStatement declarationStatement:
+                return AnalyzeDeclaration(declarationStatement, scope);
+            case ExpressionStatement expressionStatement:
+                break;
+            case IfElseStatement ifElseStatement:
+                break;
+            case ReturnStatement returnStatement:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(statement));
+        }
+
+        throw new InvalidOperationException();
+    }
+
+    private (StatementNode, Scope) AnalyzeDeclaration(DeclarationStatement declarationStatement, Scope scope)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+
+internal class DeclarationStatementNode : StatementNode
+{
+}
+
+internal class StatementNode : SemanticNode
+{
+}
+
+internal class BlockNode : SemanticNode
+{
+    public IEnumerable<StatementNode> Statements { get; }
+
+    public BlockNode(IEnumerable<StatementNode> statements)
+    {
+        Statements = statements;
+    }
+}
+
+internal class FunctionNode : SemanticNode
+{
+    public FunctionNode(string functionName, BlockNode block)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+internal class SemanticProgramNode : SemanticNode
+{
+    public SemanticProgramNode(List<FunctionNode> functions)
+    {
+        throw new NotImplementedException();
+    }
+}
